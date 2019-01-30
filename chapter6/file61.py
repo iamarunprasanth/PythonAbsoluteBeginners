f030f6398b58a02e12cbf6d72c32c9a71d06a797
arun pasupathy <iamarunprasanth@gmail.com>
2019-01-30 19:48:58 +0530

    Chapter -6 In progress

---
+++ chapter6/file61.py
@@ -0,0 +1,7 @@
+# Scope of the variable
+"""
+GLOBAL - can be accessed anywhere from the program
+LOCAL - local to a function or a method
+# when var_name is same for both Global and local reference -
+Use <Global> reference on a local var_name to treat as Global
+"""
